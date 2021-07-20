5e9e261fb008fd6965393af05e99d3c8705deff3
girish-vk <87634351+girish-vk@users.noreply.github.com>
2021-07-20 11:29:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 location = []
 @app.route('/')
 def homepage():
-    return "My first"
+    return "My first App"
 @app.route('/location',methods=['GET'])
 def get():
     return str(location)
